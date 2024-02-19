e9f4a376a9eba5989b2ecc8338a1148be7b21c51
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2024-02-20 00:49:37 +0600

    set-up complete

--- bd-client/src/Routes/Route.jsx
+++ bd-client/src/Routes/Route.jsx
@@ -1,9 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
