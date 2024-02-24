66d4502910c3bc6881d1e8fa5583e3e5d8ecc64c
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2024-02-24 10:12:52 +0600

    sign up complete

--- bd-client/src/components/SafetyZone/Signup.jsx
+++ bd-client/src/components/SafetyZone/Signup.jsx
@@ -1,9 +1,183 @@
-import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { ImSpinner4 } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
+import { useContext } from "react";
+
+import toast from "react-hot-toast";
+// import { saveUser } from "../../Api/auth";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.pathname || "/";
+  const {
+    setLoading,
+    createUser,
+    signInWithGoogle,
+    updateUserProfile,
+    loading,
+  } = useContext(AuthContext);
+
+  //Handle SignUp Function
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const name = event.target.name.value;
+    const image = event.target.photo.value;
+
+    //image upload
+
+    createUser(email, password)
+      .then((loggedUser) => {
+        // console.log(loggedUser.user);
+        updateUserProfile(name, image)
+          .then(() => {
+            //save the user to dataBase
+
+            toast.success(`SignUp successfull`);
+            // saveUser(loggedUser?.user);
+          })
+          .catch((err) => {
+            setLoading(false);
+            console.log(err.message);
+            toast.error(err.message);
+          });
+        navigate(from, { replace: true });
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err.message);
+        toast.error(err.message);
+      });
+  };
+  //handle google signin
+  const handleGoogleSign = () => {
+    signInWithGoogle()
+      .then((result) => {
+        console.log(result?.user);
+        //save the user to dataBase
+        saveUser(result?.user);
+        navigate(from, { replace: true });
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err.message);
+        toast.error(err.message);
+      });
+  };
   return (
-    <div>
-      <h1>Nothing Added Here</h1>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col max-w-md px-6 rounded-md sm:p-10 bg-gray-100 text-gray-900">
+        <div className="mb-4 text-center">
+          <h1 className="my-2 text-4xl font-bold">Sign Up</h1>
+          <p className="text-sm text-gray-400">Welcome to Solution BD IT</p>
+        </div>
+        <form
+          onSubmit={handleSubmit}
+          noValidate=""
+          action=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
+        >
+          <div className="space-y-4">
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter Your Name Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                photo
+              </label>
+              <input
+                type="text"
+                name="photo"
+                id="photo"
+                placeholder="Enter Your Photo URL Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className="block mb-2 text-sm">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                required
+                placeholder="Enter Your Email Here"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+                data-temp-mail-org="0"
+              />
+            </div>
+            <div>
+              <div className="flex justify-between">
+                <label htmlFor="password" className="text-sm mb-2">
+                  Password
+                </label>
+              </div>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                required
+                placeholder="*******"
+                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-rose-500 bg-gray-200 text-gray-900"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="bg-blue-500 w-full rounded-md py-3 text-white"
+            >
+              {loading ? (
+                <ImSpinner4 size={24} className="m-auto animate-spin" />
+              ) : (
+                "Continue"
+              )}
+            </button>
+          </div>
+        </form>
+        <div className="flex items-center pt-4 space-x-1">
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+          <p className="px-3 text-sm dark:text-gray-400">
+            Signup with social accounts
+          </p>
+          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
+        </div>
+        <div
+          onClick={handleGoogleSign}
+          className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
+        >
+          <FcGoogle size={32} />
+
+          <p>Continue with Google</p>
+        </div>
+        <p className="px-6 text-sm text-center text-gray-400">
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            className="hover:underline hover:text-rose-500 text-gray-600"
+          >
+            Login
+          </Link>
+          .
+        </p>
+      </div>
     </div>
   );
 };
